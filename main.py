5f844af466070f0ddfba1465124809f9658f560d
GregMorita <gmorita2003@gmail.com>
2025-02-25 00:22:12 -0800

    added data files

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import numpy as np
-
 #cyptoscape / networkx
+
+# Random walks with restarts 
 def rand_walk_restarts(graph):
+    #have some gamma value between 0 and 1 for restart chance,
+    pass
+
+def generate_matrix():
     pass
 
 def main():
-    pass
+    generate_matrix()
 
 if __name__ == '__main__': 
     main()
