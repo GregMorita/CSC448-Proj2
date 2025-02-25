913d77ed0691d6512ec0494a68a59d979c6ea1ac
GregMorita <gmorita2003@gmail.com>
2025-02-25 02:21:42 -0800

    worked on rand walk

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import numpy as np
 #cyptoscape / networkx
 
 # Random walks with restarts 
-def rand_walk_restarts(graph):
+def rand_walk_restarts(graph, node, gamma):
     #have some gamma value between 0 and 1 for restart chance,
+    curr = node
+    while True:
+        sum = 0
+        choices = {}
+        for i in range(graph[curr]):
+            sum += graph[curr][i]
+            choices.update({i : graph[curr][i]})
+        
+
+        #do something until we break
+        pass
     pass
 
 def generate_matrix():
     pass
 
 def main():
-    generate_matrix()
+    #generate_matrix()
+    pass
 
 if __name__ == '__main__': 
     main()
